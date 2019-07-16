5dee3e15fc63e447fbe203e9d0a7619ef1b568af
Michael Gatewood <michael@mngatewood.com>
2019-07-16 17:32:02 -0600

    Add helpers to accounts js

--- imports/ui/accounts/accounts.js
+++ imports/ui/accounts/accounts.js
@@ -1,1 +1,18 @@
-Meteor.subscribe('accounts');
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import Accounts from '../../api/accounts/accounts.js';
+import './account.html';
+import './accounts.html';
+
+Template.accounts.onCreated(function accountsOnCreated() {
+  this.state = new ReactiveDict();
+  Meteor.subscribe('accounts')
+})
+
+Template.accounts.helpers({
+  accounts() {
+    return Accounts.find({});
+  },
+});
+

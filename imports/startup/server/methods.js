e1852baef239ac933b8d16eb579ce4e9dff592ab
Michael Gatewood <michael@mngatewood.com>
2019-07-17 09:34:36 -0600

    Add sufficient funds validation and both accounts exist

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -8,11 +8,19 @@ Meteor.methods({
     let toBalance = toAccount.bal;
     let amount = transaction.amount;
 
+    if(amount > fromBalance) {
+      throw new Meteor.Error('insufficient funds');
+    }
+
     transaction.fromAccount = fromAccountName
     transaction.toAccount = toAccountName
 
-    Accounts.update(fromAccount, { $set: { bal: fromBalance - amount }})
-    Accounts.update(toAccount, { $set: { bal: toBalance + amount }})
-    Transactions.insert(transaction)
+    if(fromAccount && toAccount) {
+      Accounts.update(fromAccount, { $set: { bal: fromBalance - amount }})
+      Accounts.update(toAccount, { $set: { bal: toBalance + amount }})
+      Transactions.insert(transaction)
+    } else {
+      throw new Meteor.Error('unable to find the accounts');
+    }
   }
 })
